f31b0e7e5adb14be879d7a399f22ba931cc8dba2
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-07-15 11:42:13 +0300

    Add login validation

--- forum-app/src/app/user/user.module.ts
+++ forum-app/src/app/user/user.module.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, UserRoutingModule, FormsModule, SharedModule],
 })
 export class UserModule {}
