a6c75c047fa059b63b4a4c76370a4155824fb84b
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-07-17 00:12:23 +0300

    Add new theme validation

--- forum-app/src/app/theme/theme.module.ts
+++ forum-app/src/app/theme/theme.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { ThemeRoutingComponent } from './theme.routing.module';
 import { DetailedThemeComponent } from './detailed-theme/detailed-theme.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NewThemeComponent, DetailedThemeComponent],
-  imports: [CommonModule, ThemeRoutingComponent],
+  imports: [CommonModule, ThemeRoutingComponent, FormsModule],
 })
 export class ThemeModule {}
