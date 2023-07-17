161c532629da7b92743d574e991797f55dcbefb9
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-07-17 18:39:05 +0300

    Add profile edit update

--- forum-app/src/app/user/profile/profile.component.ts
+++ forum-app/src/app/user/profile/profile.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
 
+interface Profile {
+  username: string;
+  email: string;
+  tel: string;
+}
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  isEditMode: boolean = false;
 
+  profileDetails: Profile = {
+    username: 'Peter',
+    email: 'peter@gmail.com',
+    tel: '993 451 122',
+  };
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: [
+      '',
+      [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)],
+    ],
+    tel: [''],
+  });
+
+  constructor(private fb: FormBuilder) {}
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+  }
+  saveNewProfileData(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    this.profileDetails = { ...(this.form.value as Profile) };
+
+    this.toggleEditMode();
+  }
 }
